7ee81da8d38ac29452698b27551ca2fb1e46aa0c
gustavoRuiz <jimgustavo1987@hotmail.com>
2019-01-08 20:07:21 -0500

    Success applying filter and map method

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import Axios from "axios";
 
 class App extends React.Component {
-  state = {
-    stock: [
-      { productName: "ipadPRO", keywords: "TABLET" },
-      { productName: "Iphone X", keywords: "CELLPHONE" },
-      { productName: "Samsung Galaxi Tab", keywords: "TABLET" },
-      { productName: "Samsung S9", keywords: "CELLPHONE" }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      stock: [
+        { productName: "ipadPRO", keywords: "TABLET" },
+        { productName: "Iphone X", keywords: "CELLPHONE" },
+        { productName: "Samsung Galaxi Tab", keywords: "TABLET" },
+        { productName: "Samsung S9", keywords: "CELLPHONE" },
+        { productName: "ASUS", keywords: ["TABLET", "LAPTOP"] }
+      ]
+    };
+  }
 
   componentDidMount() {
     Axios.get("/api/stock")
       .then(res => {
         const stock = res.data;
         this.setState({ stock });
+        console.log({ stock });
       })
       .catch(function(error) {
         console.log(error);
@@ -23,34 +28,40 @@ class App extends React.Component {
   }
 
   render() {
+    var key = "SOFTWARE";
+    let tag = this.state.stock.filter(
+      ({ productName, description, keywords, image, price }) =>
+        keywords == `${key}`
+    );
+
+    console.log(tag);
+
     return (
       <div className="grid-container">
         <input
           type="text"
-          //  value={this.state.filteredStock}
-          //  onChange={this.filteredStock.bind(this)}
+          //  value={this.state.search}
+          //  onChange={this.updateSearch.bind(this)}
         />
+        <button onClick={null}>LAPTOPS</button>
+        <button onClick={null}>SOFTWARE</button>
         <div className="grid">
-          {this.state.stock.map(
-            ({ productName, description, keywords, image, price }) => (
-              <div className="card">
-                <h2>{productName}</h2>
-                <img
-                  src={image}
-                  title={productName}
-                  height={180}
-                  width={180}
-                  alt={"Sorry!, this toy has an error!"}
-                />
-                <div>{description}</div>
-                <div>{keywords}</div>
-                <div>{price}</div>
-                <button>Comprar</button>
-              </div>
-            )
-          )
-          //.filter(({ keywords }) => keywords !== "CELULARES")
-          }
+          {tag.map(({ productName, description, keywords, image, price }) => (
+            <div className="card">
+              <h2>{productName}</h2>
+              <img
+                src={image}
+                title={productName}
+                height={180}
+                width={180}
+                alt={"Sorry!, this toy has an error!"}
+              />
+              <div>{description}</div>
+              <div>{keywords}</div>
+              <div>{price}</div>
+              <button>Comprar</button>
+            </div>
+          ))}
         </div>
       </div>
     );
